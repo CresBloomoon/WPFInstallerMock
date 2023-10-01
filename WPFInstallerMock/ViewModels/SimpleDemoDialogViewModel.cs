6acc4400da02a38e7895eddec02061e67ca8b165
CresBloomoon <metroidm2421@gmail.com>
2023-10-01 18:13:05 +0900

    プロジェクト ファイルを追加します。

---
+++ WPFInstallerMock/ViewModels/SimpleDemoDialogViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MvvmWizard.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+
+namespace WPFInstallerMock.ViewModels {
+    public sealed class SimpleDemoDialogViewModel {
+
+        public SimpleDemoDialogViewModel() {
+            var unityContainer = new UnityContainer();
+            WizardSettings.Instance.ViewResolver = type => unityContainer.Resolve(type);
+        }
+    }
+}
---
+++ WPFInstallerMock/Views/SimpleDemoDialog.xaml.cs
@@ -0,0 +1,36 @@
+﻿using MvvmWizard.Classes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WPFInstallerMock.Views {
+    /// <summary>
+    /// SimpleDemoDialog.xaml の相互作用ロジック
+    /// </summary>
+    public partial class SimpleDemoDialog {
+        public SimpleDemoDialog() {
+
+            CloseCommand = new SimpleGenericCommand<Dictionary<string, object>>(ExecuteMethod);
+            SharedContext = new Dictionary<string, object>();
+            SharedContext["In"] = 88;
+
+            InitializeComponent();
+        }
+
+        private void ExecuteMethod(Dictionary<string, object> obj) {
+            Close();
+        }
+        public ICommand CloseCommand { get; }
+        public Dictionary<string, object> SharedContext { get; }
+    }
+}
